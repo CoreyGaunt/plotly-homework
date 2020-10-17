f8bf76e85bbe0b56811c15869fd7e8080aa7fb2b
CoreyGaunt <65140255+CoreyGaunt@users.noreply.github.com>
2020-10-17 15:50:20 -0500

    finishing the bonus portion of my plotly hw

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,21 +2,21 @@ function retrievePlot(name) {
 
     // getting data from json file
     d3.json("samples.json").then((data) => {
-        console.log(data)
 
     var samples = data.samples.filter(d => d.id === name)[0];
-        console.log(samples);
 
     var sample_values = samples.sample_values.slice(0,10).reverse();
-        console.log(sample_values);
     
     var topValues = samples.otu_ids.slice(0,10).reverse();
-        console.log(topValues);
 
     var otu_id = topValues.map(d => "OTU " + d)
-        console.log(otu_id)
     
     var labels = samples.otu_labels.slice(0,10);
+
+    var wash = data.metadata.filter(d => d.id.toString() === name)[0];
+
+    var washFreq = wash.wfreq;
+
 
     // bar chart
     var trace = {
@@ -24,7 +24,7 @@ function retrievePlot(name) {
         y: otu_id,
         text: labels,
         marker: {
-            color: 'red'},
+            color: '#3CB371'},
             type: "bar",
             orientation: "h"
         };
@@ -54,7 +54,19 @@ function retrievePlot(name) {
         mode: "markers",
         marker: {
             size: samples.sample_values,
-            color: samples.otu_ids
+            color: [
+                '#0000ff',
+                '#0000e5',
+                '#0000cc',
+                '#0000b2',
+                '#000099',
+                '#00007f',
+                '#000066',
+                '#00004c',
+                '#000033',
+                '#000019',
+                '#000000'
+            ]
         },
         text: samples.otu_labels
     };
@@ -62,6 +74,7 @@ function retrievePlot(name) {
     var layout2 = {
         title:"Top 10 OTUs Bubble Chart",
         xaxis:{title: "OTU ID"},
+        yaxis:{title: "Sample Values"},
         height: 600,
         width: 1000
     };
@@ -69,7 +82,50 @@ function retrievePlot(name) {
     var data2 = [trace2];
 
     Plotly.newPlot("bubble",data2,layout2);
-    });
+
+    // gauge chart
+
+    var trace3 = {
+        domain: {
+            x: [0, 1],
+            y: [0, 1]
+        },
+        value: parseFloat(washFreq),
+        title: { text: "Wash Frequency" },
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+            axis: {range: [null, 9], tickwidth:1, tickcolor:"maroon"},
+            bar: {color:"gold"},
+            steps: [
+                { range: [0, 1], color: "#E6E6FA"},
+                { range: [1, 2], color: "#D8BFD8"},
+                { range: [2, 3], color: "#DA70D6"},
+                { range: [3, 4], color: "#BA55D3"},
+                { range: [4, 5], color: "#9400D3"},
+                { range: [5, 6], color: "#8B008B"},
+                { range: [6, 7], color: "#9370DB"},
+                { range: [7, 8], color: "#8A2BE2"},
+                { range: [8, 9], color: "#4B0082"},
+            ] 
+        }
+    };
+
+    var layout3 = {
+        width: 600,
+        height: 500,
+        margin: {
+            t:0,
+            b:0
+        }
+    };
+
+    var data3 = [trace3];
+    
+    Plotly.newPlot("gauge",data3,layout3);
+});
+
+    
 }
 
 
@@ -79,6 +135,7 @@ function retrieveInfo(name) {
         console.log(metadata)
         var result = metadata.filter(d => d.id.toString() === name)[0];
         var info = d3.select('#sample-metadata');
+        info.html("");
         Object.entries(result).forEach((d) => {
             info.append("h6").text(d[0].toUpperCase() + ": " + d[1] + "\n");
         });
